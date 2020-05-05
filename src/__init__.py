7638f7444a94aaa023af5335a42747923d1b0176
Michael Manhard <mmanhard93@gmail.com>
2020-05-05 18:19:21 -0500

    -Add and configure mongoengine.

--- src/__init__.py
+++ src/__init__.py
@@ -1,21 +1,33 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+from flask_mongoengine import MongoEngine
 import os
+from mongoengine import *
 
-from .routes import auth, users
+from .routes import auth, users, entries
+from .models.user import User
+from .models.entry import Entry
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        MONGO_URI="mongodb://localhost:27017/pain_control_v1",
-    )
+    # app.config.['SEC'](
+    #     SECRET_KEY='dev'
+    # )
 
-    mongo = PyMongo(app)
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'test2',
+        'host': 'mongodb://localhost:27017/pain_control_v1'
+    }
 
     app.register_blueprint(auth.bp)
     app.register_blueprint(users.bp)
+    app.register_blueprint(entries.bp)
 
     return app
 
+app = create_app()
+db = MongoEngine(app)
+
+# test = User.objects()
+# for user in test:
+#     user.delete()
