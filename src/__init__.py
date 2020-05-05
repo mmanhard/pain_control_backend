9f6b5892d7f1f13d934732d344b3c431465c501a
Michael Manhard <mmanhard93@gmail.com>
2020-05-05 18:19:54 -0500

    -Use dev secret key.

--- src/__init__.py
+++ src/__init__.py
@@ -10,10 +10,7 @@ from .models.entry import Entry
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.['SEC'](
-    #     SECRET_KEY='dev'
-    # )
-
+    app.config['SECRET_KEY'] = 'dev'
     app.config['MONGODB_SETTINGS'] = {
         'db': 'test2',
         'host': 'mongodb://localhost:27017/pain_control_v1'
