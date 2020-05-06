ba1d34eb610beced1cc10c2f93cbf4f950d0033a
Michael Manhard <mmanhard93@gmail.com>
2020-05-06 12:46:22 -0500

    -Add config file.

---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+SECRET_KEY = 'dev'
+MONGODB_SETTINGS = { 'host': 'mongodb://localhost:27017/pain_control_v2' }
--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
 
-def create_app(test_config=None):
+def create_app():
     from . import models, routes
 
     # Create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config['SECRET_KEY'] = 'dev'
-    app.config['MONGODB_SETTINGS'] = {
-        'host': 'mongodb://localhost:27017/pain_control_v2'
-    }
+    app = Flask(__name__)
+    app.config.from_object('config')
 
     models.init_app(app)
     routes.init_app(app)
