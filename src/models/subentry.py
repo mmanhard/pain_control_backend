d153af7f5af26957d68f5288aa91397215211e80
Michael Manhard <mmanhard93@gmail.com>
2020-05-06 12:56:15 -0500

    -Add subentry class and subclasses to models.

---
+++ src/models/subentry.py
@@ -0,0 +1,28 @@
+import mongoengine as me
+
+class SubEntry(me.EmbeddedDocument):
+    notes = me.StringField()
+
+    meta = {'allow_inheritance': True}
+
+    def __repr__(self):
+        return self.notes
+
+class PainSubEntry(SubEntry):
+    body_part = me.ReferenceField('BodyPart', required=True)
+    pain_level = me.IntField(required=True)
+    life_impact = me.IntField()
+    mood_impact = me.IntField()
+
+class MoodSubEntry(SubEntry):
+    medication = me.StringField(required=True)
+    med_impact = me.IntField(required=True)
+
+class MedicationSubEntry(SubEntry):
+    mood = me.StringField(required=True)
+    mood_impact = me.IntField(required=True)
+
+class ActivitySubEntry(SubEntry):
+    activity = me.StringField(required=True)
+    duration = me.IntField(required=True)
+    act_impact = me.IntField(required=True)
