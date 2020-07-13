20228e3d264ba6a1355f4fad1b12229d7c644142
Michael Manhard <mmanhard93@gmail.com>
2020-07-13 17:11:25 -0500

    -Disable retryable rewrites in mongoldb.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'some_secret_key'
 # MONGODB_SETTINGS = { 'host': 'mongodb://localhost:27017/pain_control_v2' }
-MONGODB_SETTINGS = { 'host': 'mongodb://heroku_k3q1vztw:p1i6pfil82bvp57fpgims4jha8@ds011903.mlab.com:11903/heroku_k3q1vztw' }
+MONGODB_SETTINGS = { 'host': 'mongodb://heroku_k3q1vztw:p1i6pfil82bvp57fpgims4jha8@ds011903.mlab.com:11903/heroku_k3q1vztw?retryWrites=false' }
