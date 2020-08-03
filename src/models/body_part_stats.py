ba566ce34a3e293c6462491a9488bb28e383d1ef
Michael Manhard <mmanhard93@gmail.com>
2020-08-03 18:12:29 -0500

    -Remove unused timing stats in body_part_stats model

--- src/models/body_part_stats.py
+++ src/models/body_part_stats.py
@@ -2,7 +2,6 @@ import mongoengine as me
 import json
 
 class BodyPartStats(me.EmbeddedDocument):
-    # timing_stats = me.EmbeddedDocumentListField('BodyPartStats')
     high = me.FloatField(default=0.0)
     low = me.FloatField(default=10.0)
     avg = me.FloatField(default=0.0)
@@ -18,7 +17,3 @@ class BodyPartStats(me.EmbeddedDocument):
             'avg': str(self.avg),
             'num_entries': str(self.num_entries)
         }
-
-# class TimingStats(me.EmbeddedDocument):
-#     time_of_day = me.StringField(required=True)
-#     avg = me.FloatField(required=True)
